c3f078de23f60dfce0212c0613e8d3ce92a42186
Srr233 <Sr233@yandex.ru>
2020-09-23 14:51:16 +0300

    feat: add root of number function

--- calculator/script.js
+++ calculator/script.js
@@ -17,20 +17,59 @@ class Calculator {
     addNumber(number) {
         if (number !== '.') {
             this.currentOperand += number;
-        } else if (!this.currentOperand.includes('.') && this.currentOperand !== "") {
+        } else if (!this.currentOperand.includes('.') && this.currentOperand !== "" && !this.currentOperand.includes("√")) {
             this.currentOperand += '.';
         }
     }
     addOperation (operation) {
-        if (this.currentOperand !== "") {
+        if (this.currentOperand !== "" && !this.currentOperand.includes("√")) {
             this.previousOperand = `${this.currentOperand} ${operation}`;
             this.currentOperand = '';
-        } else if (operation === "-"){
+        } else if (operation === "-" && !this.currentOperand.includes("√")){
             this.currentOperand = "- ";
+        } else if (this.currentOperand === "" && operation === "√") {
+            this.currentOperand = "√";
+        } else if (this.currentOperand.includes("√") && this.currentOperand.length !== 1) {
+            const num = +this.currentOperand.slice(1);
+            this.compute(num);
         }
     }
-    compute () {
-
+    compute (sqrt) {
+        if (this.currentOperand && this.previousOperand) {
+            const operation = this.previousOperand[this.previousOperand.length-1];
+            if (this.currentOperand.includes("-")) {}
+            const current = +this.currentOperand;
+            const previous = +this.previousOperand.slice(0, this.previousOperand.length - 2)
+            switch (operation) {
+                case "+":
+                    this.currentOperand = `${previous + current}`;
+                    this.previousOperand = "";
+                    break;
+                case "-":
+                    this.currentOperand = `${previous - current}`;
+                    this.previousOperand = "";
+                    break;
+                case "*":
+                    this.currentOperand = `${previous * current}`;
+                    this.previousOperand = "";
+                    break;
+                case "÷":
+                    this.currentOperand = `${previous / current}`;
+                    this.previousOperand = "";
+                    break;
+                case "^":
+                    this.currentOperand = `${previous ** current}`;
+                    this.previousOperand = "";
+                    break;
+            } 
+        } else if (sqrt || this.currentOperand.includes("√") && this.currentOperand.length > 1) {
+            if (sqrt) {
+                this.previousOperand = `${Math.sqrt(sqrt)}`;
+                this.currentOperand = "";
+            } else {
+                this.currentOperand = `${Math.sqrt(this.currentOperand.slice(1))}`;
+            }
+        }
     }
     updateDisplay () {
         this.currentOperandText.innerText = this.currentOperand;
@@ -68,5 +107,8 @@ allClearButton.addEventListener("click", () => {
         calculator.clear();
         calculator.updateDisplay();
 });
-equalsButton.addEventListener("click", calculator.compute);
+equalsButton.addEventListener("click", () => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
 
