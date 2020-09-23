0e60d2746e9e2a5728bc4b41a53417f720b07b90
Srr233 <Sr233@yandex.ru>
2020-09-23 11:50:16 +0300

    feat: add interface button

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+"Use strict"
+
+const numberButton = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const deleteButton = document.querySelector("[data-delete]");
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const equalsButton = document.querySelector("[data-equals]");
