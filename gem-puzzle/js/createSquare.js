f71f090f88908f62b0979b631baa421dcd9e4921
Srr233 <Sr233@yandex.ru>
2020-11-03 16:19:33 +0300

    fix: change style add and add size for img

--- gem-puzzle/js/createSquare.js
+++ gem-puzzle/js/createSquare.js
@@ -1,13 +1,14 @@
 "Use strict";
 
-function createSquare (srcImg, number, classForSq = 'square', pos = {}) {
-    if (!pos.x || !pos.y) throw new Error('need object with keys x and y position for img');
+function createSquare (srcImg, number, classForSq = 'square', pos = {}, sizeRem) {
+    if (pos.x === undefined || pos.y === undefined) throw new Error('need object with keys x and y position for img');
 
     const square = document.createElement('div');
 
     square.classList.add(classForSq);
-    square.style.background = `url(${srcImg})`;
-    square.style.backgroundPosition = `${pos.x}% ${pos.y}%`;
+    square.style.cssText = `background: url(${srcImg});
+                            background-position: ${pos.x}% ${pos.y}%;
+                            background-size: ${sizeRem}rem`;
     square.setAttribute('data-number', number);
 
     return square;
