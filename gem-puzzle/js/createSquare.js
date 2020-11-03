634f9600cf0000b5e6a81b4aac9f109f1db03e5e
Srr233 <Sr233@yandex.ru>
2020-11-03 15:08:59 +0300

    feat: implement createSquare foo

--- gem-puzzle/js/createSquare.js
+++ gem-puzzle/js/createSquare.js
@@ -1,8 +1,20 @@
 "Use strict";
 
-function createSquare (img, number, classFor = 'square') {
+function createSquare (srcImg, number, classForSq = 'square', classForImg = 'cut-img') {
+    if (!srcImg) throw new Error ("need src for image!");
+    
     const square = document.createElement('div');
-    square.classList.add(classFor)
+    const img = document.createElement('img');
+
+    img.classList.add(classForImg)
+    square.classList.add(classForSq);
+
+    square.setAttribute('data-number', number);
+    img.src = srcImg;
+
+    square.appendChild(img);
+
+    return square;
 }
 
 export {createSquare};
