9a4c7df3be14fc47fc2bd5bdaffde6f537cadd15
Srr233 <Sr233@yandex.ru>
2020-11-04 21:16:27 +0300

    refactor: refactoring createSquare

--- gem-puzzle/js/createSquare.js
+++ gem-puzzle/js/createSquare.js
@@ -1,17 +1,29 @@
 "Use strict";
 
-function createSquare (srcImg, number, classForSq = 'square', pos = {}, sizeRem) {
+function createSquare (srcImg, number, classForSq = 'square', pos = {}, sizeImg, sizeSq) {
     if (pos.x === undefined || pos.y === undefined) throw new Error('need object with keys x and y position for img');
+    
+    const squareWrapper = document.createElement('div');
+    squareWrapper.style.cssText = `position: relative;
+                                   width: ${sizeSq}px;
+                                   height: ${sizeSq}px;
+                                   margin: 0.1rem;`;
 
     const square = document.createElement('div');
+    squareWrapper.insertAdjacentElement('beforeend', square);
+
+    square.textContent = number;
 
     square.classList.add(classForSq);
-    square.style.cssText = `background: no-repeat url(${srcImg});
+    square.style.cssText = `position: absolute;
+                            width: ${sizeSq}px;
+                            height: ${sizeSq}px;
+                            background: no-repeat url(${srcImg});
                             background-position: ${pos.x}px ${pos.y}px;
-                            background-size: ${sizeRem}px`;
+                            background-size: ${sizeImg}px`;
     square.setAttribute('data-number', number);
 
-    return square;
+    return squareWrapper;
 }
 
 export {createSquare};
